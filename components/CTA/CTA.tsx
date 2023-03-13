c1f53eec303cd4ee33c7eecbc625588130463329
Nylzen <tugoresn@gmail.com>
2023-03-13 09:56:17 -0300

    Switch: CTA element to use background image

--- components/CTA/CTA.tsx
+++ components/CTA/CTA.tsx
@@ -3,17 +3,14 @@ import CTAStyles from './CTA.module.css';
 const CTA = () => {
   return (
     <section className={CTAStyles.CTASection}>
-      <h3 className={CTAStyles.CTAHeading}>
-        ¡Toma acción ahora y consigue el dinero que necesitas para tus proyectos
-        y metas!
-      </h3>
-      <p>
-        Con nuestro proceso de solicitud rápido y sin complicaciones, podemos
-        ayudarte a conseguir el préstamo personal que necesitas. ¡Haz clic aquí
-        para solicitar tu préstamo y comenzar a hacer realidad tus sueños hoy
-        mismo!
-      </p>
-      <button className='btn btn__secondary'>Solicitar Préstamo</button>
+      <div className={CTAStyles.CTA__text}>
+        <h3 className={CTAStyles.CTAHeading}>
+          ¡Toma acción ahora y consigue el dinero que necesitas para tus
+          proyectos y metas!
+        </h3>
+
+        <button className='btn btn__primary'>Solicitar Préstamo</button>
+      </div>
     </section>
   );
 };
